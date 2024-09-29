6e53206fbb7813df98b2e1b181fce8af7efbe6a9
jon4hz <me@jon4hz.io>
2024-09-29 23:29:39 +0200

    feat: use adfinis mirror

--- provisioners/update.sh
+++ provisioners/update.sh
@@ -11,8 +11,17 @@ dpkg-divert --divert /etc/apt/sources.list.d/ceph.list.distrib.disabled --rename
 echo "deb http://download.proxmox.com/debian/pve $(. /etc/os-release && echo "$VERSION_CODENAME") pve-no-subscription" >/etc/apt/sources.list.d/pve.list
 echo "deb http://download.proxmox.com/debian/ceph-reef $(. /etc/os-release && echo "$VERSION_CODENAME") no-subscription" >/etc/apt/sources.list.d/ceph.list
 
-# switch the apt mirror from us to nl.
-sed -i -E 's,ftp\.us\.debian,ftp.nl.debian,' /etc/apt/sources.list
+# switch the apt mirror to adfinis
+cat >/etc/apt/sources.list <<'EOF'
+deb http://pkg.adfinis-on-exoscale.ch/debian/ bookworm main non-free non-free-firmware contrib
+deb-src http://pkg.adfinis-on-exoscale.ch/debian/ bookworm main non-free non-free-firmware contrib
+
+deb http://security.debian.org/ bookworm-security main
+deb-src http://security.debian.org/ bookworm-security main
+
+deb http://pkg.adfinis-on-exoscale.ch/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb-src http://pkg.adfinis-on-exoscale.ch/debian/ bookworm-updates main contrib non-free non-free-firmware
+EOF
 
 # update only (no upgrade since we want to use this image to test automated cluster upgrades)
 apt-get update
